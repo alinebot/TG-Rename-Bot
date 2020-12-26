045818a4b6eed45ff828f9a982d8ec7e8a7c26df
alinebot <72397223+alinebot@users.noreply.github.com>
2020-12-26 23:30:49 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -29,7 +29,8 @@ This is a Telegram Rename Bot!
 
 --------
 
-© Support channel @botUpdatess
+Updated channel @botUpdatess.
+© support 
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
